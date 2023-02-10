d59d817b5f31adc634b0ce739c10f0ec31f68626
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-09 23:37:26 -0300

    refactor: ensure SignUpController.handle implements interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,21 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name'),
       }
     }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      }
+    }
     return {
-      statusCode: 400,
-      body: new Error('Missing param: email'),
+      statusCode: 200,
+      body: 'ok',
     }
   }
 }
